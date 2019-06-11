6eebaa50b27f0c953b1b81bbcdfb4d833d31bcd7
Erick Rosete Beas <erickrosetebeas@hotmail.com>
2019-06-10 20:43:20 -0700

    about and projects in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,11 @@ import banner1200w from "../images/index/banner-1200w.jpg";
 import banner2500w from "../images/index/banner-2500w.jpg";
 import banner5000w from "../images/index/banner-5000w.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import about from "../images/index/about.jpg"
+import robot from "../images/index/robot.jpg";
+
+import { Container, Row, Col } from "react-bootstrap"
+
 
 import "./index.css";
 
@@ -85,8 +90,60 @@ class IndexPage extends React.Component {
               </div>
             </div>
           </div>
+
+          <div className="index-about">
+            <h1>About me</h1>
+            <div className="index-about-info">
+              <div>
+                <img src={about} alt="Erick Rosete" />
+              </div>
+              <div className="index-about-text">
+                <p>Versatile mechatronics engineer, capable of solving complex problems in a fast-paced environment, committed to the highest levels of professional and personal excellence, willing to develop professionally, looking for continuous learning and improvement.</p>
+                <p>Interested in Robotics, Automation and Programming.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="index-projects">
+            <p>I like to work in different things, take a look</p>
+            <h1>Projects</h1>
+            <Container>
+              <Row>
+                <Col xs={12} md={6}>
+                  <div className="project-card">
+                    <img src={robot} alt="placeholder" />
+                    <div className="sec-cover"></div>
+                    <h2>Robotics</h2>
+                  </div>
+                </Col>
+                <Col xs={12} md={6}>
+                  <div className="project-card">
+                    <img src={robot} alt="placeholder" />
+                    <div className="main-cover"></div>
+                    <h2>Web & mobile</h2>
+                  </div>
+                </Col>
+                <Col xs={12} md={6}>
+                  <div className="project-card">
+                    <img src={robot} alt="placeholder" />
+                    <div className="main-cover"></div>
+                    <h2>Games</h2>
+                  </div>
+                </Col>
+                <Col xs={12} md={6}>
+                  <div className="project-card">
+                    <img src={robot} alt="placeholder" />
+                    <div className="sec-cover"></div>
+                    <h2>AI</h2>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+
+          <hr></hr>
           <h1 className="index__title">Site under Construction</h1>
-          <p style={{ textAlign: "center", fontSize: "large" }}>
+          <p style={{ textAlign: "center" }}>
             Have any question or request?{" "}
             <a href="mailto:erickrosetebeas@hotmail.com" target="_top">
               Contact me!
